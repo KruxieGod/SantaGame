4fd44efb7f15783457220b0838e99cd4beadb837
KruxieGod <dooll4ik@mail.ru>
2024-03-25 23:04:23 +0300

    Pathfinder

---
+++ Santa/Assets/Scripts/PathMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+
+public class PathMovement : MonoBehaviour
+{
+    public Transform[] Paths;
+    public PlayerController _player;
+    private Coroutine _currentCoroutine;
+    private bool _isMoving;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            MoveToTransitions();
+    }
+
+    private void MoveToTransitions()
+    {
+        _isMoving = !_isMoving;
+        if (!_isMoving)
+        {
+            StopCoroutine(_currentCoroutine);
+            return;
+        }
+
+        _currentCoroutine = StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        var currentPoint = Paths.OrderBy(x => Vector3.Distance( _player.transform.position, x.position)).FirstOrDefault();
+        var nextPoint = GetNextPoint(currentPoint);
+        while (true)
+        {
+            var distance = Vector3.Distance(nextPoint.position, _player.transform.position);
+            Debug.Log("distance: " + distance);
+            if (Vector3.Distance(nextPoint.position,_player.transform.position) < 5f)
+                nextPoint = GetNextPoint(nextPoint);
+            _player.MoveTo((nextPoint.position - _player.transform.position).normalized);
+            yield return null;
+        }
+    }
+
+    private Transform GetNextPoint(Transform currentPoint)
+    {
+        for (var i = 0; i < Paths.Length; i++)
+            if (Paths[i] == currentPoint)
+                return Paths.Length <= i + 1 ? Paths[0] : Paths[i + 1];
+
+        return null;
+    }
+}
--- Santa/Assets/Scripts/Player/Movement/PlayerController.cs
+++ Santa/Assets/Scripts/Player/Movement/PlayerController.cs
@@ -13,8 +13,20 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        var direction = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-        Move(direction);
+        //var direction = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        //Move(direction);
+    }
+    
+    public void MoveTo(Vector3 input)
+    {
+        _animatorController.Animate(new Vector2(input.x, input.z));
+        if (input == Vector3.zero)
+            return;
+        Vector3 dir = input;
+        dir = new Vector3(dir.x, 0, dir.z);
+        dir.Normalize();
+        ApplyRotation(dir);
+        _characterController.Move(dir * (_speed * Time.deltaTime));
     }
 
     private void Move(Vector2 input)
@@ -28,12 +40,11 @@ public class PlayerController : MonoBehaviour
         ApplyRotation(dir);
         _characterController.Move(dir * (_speed * Time.deltaTime));
     }
-
+    
     private void ApplyRotation(Vector3 direction)
     {
         var eulerAngles = transform.eulerAngles;
         float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-        Debug.Log("Angle: " + angle);
         var targetRotation = Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.y, Quaternion.LookRotation(direction).eulerAngles.y, _speedRotation);
         var rotation = new Vector3(eulerAngles.x,targetRotation,eulerAngles.z);
         transform.eulerAngles = rotation;

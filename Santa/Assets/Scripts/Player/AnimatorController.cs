1f3c5bcb0b56585a8742fd9740c6e9e59dcf913d
KruxieGod <dooll4ik@mail.ru>
2024-04-07 21:52:29 +0300

    slide object, Timeline slide

--- Santa/Assets/Scripts/Player/AnimatorController.cs
+++ Santa/Assets/Scripts/Player/AnimatorController.cs
@@ -1,13 +1,28 @@
+using System;
 using UnityEngine;
 
 public class AnimatorController : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     private const string _locomotionAnim = "Locomotion";
-    
-    
+    private const string _runningAnim = "Running";
+    private const string _idleAnim = "Idle";
+    private const string _slideAnim = "Slide";
+
+
     public void Animate(Vector2 direction)
     {
         _animator.SetFloat(_locomotionAnim ,direction.magnitude);
     }
+
+    public void Run() {
+        _animator.CrossFade(_locomotionAnim,0.2f);
+        _animator.SetFloat(_locomotionAnim, 1);
+    }
+    public void Idle(){
+        _animator.CrossFade(_locomotionAnim,0.2f);
+        _animator.SetFloat(_locomotionAnim, 0);
+    }
+    public void Slide() => _animator.CrossFade(_slideAnim,0.2f);
+    
 }
